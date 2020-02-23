10822ac5c928589ea140c3e515a150377ea266b8
tones31 <anthagostino@gmail.com>
2020-02-23 13:41:49 -0500

    1.0.3
    - removed /dist from ignore

--- build.js
+++ build.js
@@ -1,6 +1,9 @@
 const Build = require('@voliware/node-build').Build;
+const version = require('./package.json').version;
 
 new Build({
+    name: "Event System",
+    version: version,
     input: "./lib/eventSystem.js",
     output: "./dist/eventSystem.min.js",
     minify: true
